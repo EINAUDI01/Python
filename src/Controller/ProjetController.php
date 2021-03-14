4c2a09bf0a9ea26b78e055ab5933f609d56b05be
EINAUDI01 <einaudikanhonou2019@gmail.com>
2021-03-15 00:01:48 +0100

    controleur et accueil

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,5 +33,22 @@ class ProjetController extends AbstractController
     public function creer_utilisateur() : Response
     {
         return $this->render('projet/creer-utilisateur.html.twig');
+    }
+	
+	/**
+     * @Route("/login", name="Create_login")
+    */
+    public function Createlogin (Request $request,EntityManagerInterface $manager) : Response
+    {
+		$login=$request->request->get("nom");
+		$password=$request->request->get("password");
+		
+		if ($login=="admin")
+			$message="BIENVENUE CHER ADMINISTRATEUR";
+		else
+			$message="BIENVENUE CHER UTILISATEUR";
+		
+		
+        return $this->render('login/Creerlogin.html.twig', [ 'nom' => $login, 'message' => $message, ] );
     }
 }
