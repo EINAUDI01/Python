b38a25359b17fa56d3c8f9c656f711ac8a5bce92
EINAUDI01 <einaudikanhonou2019@gmail.com>
2022-02-18 16:00:15 +0100

    exo3

---
+++ exo3.py
@@ -0,0 +1,39 @@
+from re import I
+
+def sommePuissanceDeDeux(min, j):
+    listeSommePuissance = []
+    listeSommePuissance.append(min) 
+    res = min - j
+    listeSommePuissance.append(res)
+    while res != 0:
+        res = min - j
+        listeSommePuissance.append(res)
+    return listeSommePuissance
+
+
+print("Entrez le premier nombre : ")
+a=int(input())
+print("Entrez le second nombre : ")
+b=int(input())
+
+i = 2
+j=2
+liste = []
+
+#Pour determiner la plus grande puissance de 2
+if a < b:
+    while i < b:
+        min = a
+        j = i 
+        i = i ** 2
+else:
+    while i < a:
+        min = a
+        j = i 
+        i = i ** 2
+    
+liste = sommePuissanceDeDeux(min, j)
+print(liste)  
+
+
+ 
