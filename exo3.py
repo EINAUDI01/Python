b6287c5fa453c374de6c98ada1cba314816ec364
EINAUDI01 <einaudikanhonou2019@gmail.com>
2022-02-22 21:04:21 +0100

    exo3

--- exo3.py
+++ exo3.py
@@ -1,30 +1,33 @@
 def PuissanceDeDeux(min):
-    i=0
+    i = 1
+    j = 1
+    puiss = 2
+    liste=[]
     #Pour determiner la plus grande puissance de 2
-    while i <= min:
-        j = i 
-        i = i ** 2
-    return j
+    while (puiss != 1):
+        while i <= min:
+            j = i 
+            i = i * 2
+        puiss = j
+        liste.append(j)
+        min = min - puiss   
+    return liste
 
+listePuissance=[]
 print("Entrez le premier nombre : ")
 a=int(input())
+print(a)
 print("Entrez le second nombre : ")
 b=int(input())
+print(b)
 
-liste = []
-
-#Pour determiner la plus grande puissance de 2
-if a < b:
+if a < b: 
     min = a
 else:
-    min = b
+    min = b 
+    
+listePuissance = PuissanceDeDeux(min)
 
-puissanceDeDeux = PuissanceDeDeux(min)
-liste.append(puissanceDeDeux)
-
-print(liste)
-
- 
-
+print(listePuissance)
 
  
